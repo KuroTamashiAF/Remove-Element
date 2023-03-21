d7ec3737c606cc7fe257bda5d654c86c7237183b
Fedorov <chugun777akk@mail.ru>
2023-03-21 09:02:08 +0700

    all test is done. but i have a error

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include <vector>
+#include<iterator>
 using namespace std;
 
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-
-
-
-
-
+    static int removeElement(vector<int>& nums, int val) 
+    {
+        vector<int>::iterator it; 
+        it = nums.begin();
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == val)
+            {
+                nums.erase(it);
+                it--;
+                i--;
+            }
+            it++;
+        }
+     
+        return nums.size();
     }
 };
 
 
-
-
-
-
-
-
 int main()
 {
+    vector<int> num = {2,2,3 };
+    int value = 3;
+    cout<<Solution::removeElement(num, value);
 
+    return 0;
 }
