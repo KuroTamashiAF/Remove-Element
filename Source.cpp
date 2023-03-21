6a0028ff1f7cf7157f7fa42c394a8598b71dbcce
Fedorov <chugun777akk@mail.ru>
2023-03-21 07:14:56 +0700

    create task

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+
+
+
+
+    }
+};
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
